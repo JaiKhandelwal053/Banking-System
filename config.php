686cc099f18dc6c0f5f31da8a12cc73deca0e298
Jai Khandelwal <jaikhandelwal0987@gmail.com>
2021-08-17 16:24:35 +0530

    Initial commit

---
+++ config.php
@@ -0,0 +1,71 @@
+
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "bank_db";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if(!$conn){
+	die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
+}
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $servername = "localhost";
+// $username = "id17428995_mybankdatadb";
+// $password = "Zr8UmvLfjY-=iLd+";
+// $dbname = "id17428995_bank_db";
